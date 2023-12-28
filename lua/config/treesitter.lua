d2c36c61beb49123052b6811cc8b26ddbcdc7db6
rqyang <yang.ruoqi@outlook.com>
2023-12-29 07:23:09 +0800

    ensure vimdoc treesitter parser installed
    The Neovim bundled vimdoc parser under /usr/lib/nvim/parser (vimdoc.so) is incompatible with
    treesitter, causing error when help files are opened. Installing a treesitter parser will solve it.

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,8 +1,7 @@
 require("nvim-treesitter.configs").setup {
-  ensure_installed = { "python", "cpp", "lua", "vim", "json", "toml" },
+  ensure_installed = { "python", "cpp", "lua", "vim", "json", "toml", "vimdoc" },
   ignore_install = {}, -- List of parsers to ignore installing
   highlight = {
     enable = true, -- false will disable the whole extension
-    disable = { 'help' }, -- list of language that will be disabled
   },
 }
