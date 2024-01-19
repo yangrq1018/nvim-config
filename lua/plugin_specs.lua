8d06cbaf522650504cb2978d594b0aeac15757ab
martin <yang.ruoqi@outlook.com>
2024-01-19 15:57:40 +0800

    colorizer config in its own file

--- lua/plugin_specs.lua
+++ lua/plugin_specs.lua
@@ -668,25 +668,7 @@ local plugin_specs = {
     "NvChad/nvim-colorizer.lua",
     event = {"VeryLazy"},
     config = function()
-      require'colorizer'.setup{
-        filetypes = {"*"},
-        user_default_options = {
-          RGB = true, -- #RGB hex codes
-          RRGGBB = true, -- #RRGGBB hex codes
-          names = false, -- "Name" codes like Blue or blue
-          RRGGBBAA = true, -- #RRGGBBAA hex codes
-          AARRGGBB = false, -- 0xAARRGGBB hex codes
-          rgb_fn = false, -- CSS rgb() and rgba() functions
-          hsl_fn = false, -- CSS hsl() and hsla() functions
-          css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-          css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-          mode = "background", -- Set the display mode.
-          tailwind = false, -- Enable tailwind colors
-          sass = { enable = false, parsers = { "css" }, }, -- Enable sass colors
-          virtualtext = "■",
-          always_update = false
-        }
-      }
+      require("config.colorizer")
     end,
   },
 }
