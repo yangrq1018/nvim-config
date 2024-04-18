2a2108cb5b2d1725ab285a1d6e91b52ed8f4742f
martin <yang.ruoqi@outlook.com>
2024-04-18 18:59:26 +0800

    more filetype detection

--- lua/filetype.lua
+++ lua/filetype.lua
@@ -1,10 +1,21 @@
 -- Using vim.filetype lua is modern
--- Equivalent to autocmd BufRead,BufNewFile *.njk set filetype=ninja
+-- Equivalent to autocmd BufRead,BufNewFile *.njk set filetype=html
 
+-- match by extension, filename or pattern
 vim.filetype.add({
   extension = {
     njk = 'html',
     ipynb = 'jupyter',
     rasi = 'rasi',
-  }
+  },
+  pattern = {
+    -- set a dummy filetype for swaylock config file so we have color
+    -- highlights
+    ['.*/swaylock/.swaylock/config'] = 'text',
+  },
 })
+
+-- The square bracket syntax around table keys indicates that
+-- a string should serve as a key, rather than an array element.
+-- Also, it allows use like `[ name ] = 'bar'`, where name is
+-- evaluated to its value to be treated as a key.
