c2a3a1b291821e265c40587c2b87722c403a2fd5
martin <yang.ruoqi@outlook.com>
2024-07-20 14:45:36 +0800

    update which-key config for new version

--- lua/config/which-key.lua
+++ lua/config/which-key.lua
@@ -1,4 +1,6 @@
 require("which-key").setup {
+  -- disable warnings about legacy mapping specs (dap, xmake)
+  notify = false,
   plugins = {
     marks = true, -- shows a list of your marks on ' and `
     registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
@@ -18,26 +20,24 @@ require("which-key").setup {
       g = true, -- bindings for prefixed with g
     },
   },
-  -- add operators that will trigger motion and text object completion
-  -- to enable all native operators, set the preset / operators plugin above
-  operators = { gc = "Comments" },
-  key_labels = {
-    -- override the label used to display some keys. It doesn't effect WK in any other way.
-    -- For example:
-    -- ["<space>"] = "SPC",
-    -- ["<cr>"] = "RET",
-    -- ["<tab>"] = "TAB",
+  -- operators = { gc = "Comments" }, -- TODO
+  replace = {
+    key = {
+      { "<space>", "SPC" },
+      { "<cr>", "RET" },
+      { "<tab>", "TAB" },
+    },
   },
   icons = {
     breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
     separator = "➜", -- symbol used between a key and it's label
     group = "+", -- symbol prepended to a group
   },
-  window = {
-    border = "none", -- none, single, double, shadow
-    position = "top", -- bottom, top
-    margin = { 0, 0, 0, 0 }, -- extra window margin [top, right, bottom, left]
-    padding = { 1, 0, 1, 0 }, -- extra window padding [top, right, bottom, left]
+  win = {
+    border = "none",
+    -- position = "top", -- TODO
+    -- margin = { 0, 0, 0, 0 }, -- TODO
+    padding = { 1, 0 },
   },
   layout = {
     height = { min = 1, max = 25 }, -- min and max height of the columns
@@ -45,16 +45,11 @@ require("which-key").setup {
     spacing = 1, -- spacing between columns
     align = "center", -- align columns left, center or right
   },
-  ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
-  hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
+  -- option is deprecated and removed
+  -- hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
   show_help = true, -- show help message on the command line when the popup is visible
-  triggers = "auto", -- automatically setup triggers
-  -- triggers = {"<leader>"} -- or specify a list manually
-
-  triggers_blacklist = {
-    -- list of mode / prefixes that should never be hooked by WhichKey
-    -- this is mostly relevant for key maps that start with a native binding
-    -- most people should not need to change this
-    n = { "o", "O" },
+  triggers = {
+    -- automatically setup triggers
+    { "<auto>", mode = "nixsotc" },
   },
 }
