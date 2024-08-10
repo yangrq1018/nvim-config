1b19daf4477a023a7ca5158ac926c338d638e1e1
martin <yang.ruoqi@outlook.com>
2024-08-10 14:54:48 +0800

    refactor statusline component virtual_env

--- lua/config/statusline.lua
+++ lua/config/statusline.lua
@@ -104,18 +104,17 @@ local virtual_env = function()
   end
 
   local conda_env = os.getenv('CONDA_DEFAULT_ENV')
-  local venv_path = os.getenv('VIRTUAL_ENV')
+  if conda_env ~= nil then
+    return string.format("  %s (conda)", conda_env)
+  end
 
-  if venv_path == nil then
-    if conda_env == nil then
-      return ""
-    else
-      return string.format("  %s (conda)", conda_env)
-    end
-  else
+  local venv_path = os.getenv('VIRTUAL_ENV')
+  if venv_path ~= nil then
     local venv_name = vim.fn.fnamemodify(venv_path, ':t')
     return string.format("  %s (venv)", venv_name)
   end
+
+  return ""
 end
 
 local xmake_component = {
