c480ef78f0dbe1c1e2970ae99fbdc80f1e6765c3
martin <yang.ruoqi@outlook.com>
2025-02-06 13:58:37 +0800

    lsp: remove pyright configuration

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -159,15 +159,6 @@ else
   vim.notify("pylsp not found!", vim.log.levels.WARN, { title = "Nvim-config" })
 end
 
--- if utils.executable('pyright') then
---   lspconfig.pyright.setup{
---     on_attach = custom_attach,
---     capabilities = capabilities
---   }
--- else
---   vim.notify("pyright not found!", vim.log.levels.WARN, {title = 'Nvim-config'})
--- end
-
 if utils.executable("ltex-ls") then
   lspconfig.ltex.setup {
     on_attach = custom_attach,
