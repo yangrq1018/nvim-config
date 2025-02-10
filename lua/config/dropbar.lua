20c23d4d6e77ded884bc79099c7b55041552cbf4
martin <yang.ruoqi@outlook.com>
2025-02-10 13:36:41 +0800

    dropbar: disable sources.treesitter

--- lua/config/dropbar.lua
+++ lua/config/dropbar.lua
@@ -20,6 +20,28 @@ require('dropbar').setup({
       -- The default values are 'BufWinEnter', 'BufWritePost'
       'BufWinEnter',
     },
-    enable = enable
+    enable = enable,
+    sources = function(buf, _)
+      local sources = require('dropbar.sources')
+      local utils = require('dropbar.utils')
+      if vim.bo[buf].ft == 'markdown' then
+        return {
+          sources.path,
+          sources.markdown,
+        }
+      end
+      if vim.bo[buf].buftype == 'terminal' then
+        return {
+          sources.terminal,
+        }
+      end
+      return {
+        sources.path,
+        utils.source.fallback({
+          sources.lsp,
+          -- sources.treesitter,
+        }),
+      }
+    end
   }
 })
