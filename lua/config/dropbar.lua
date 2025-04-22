29c5f4f31b186126bc71888a4bd9b1afc118b7f2
martin <yang.ruoqi@outlook.com>
2025-04-22 18:59:24 +0800

    remove dropbar sources config as upstream has fixed python module issue

--- lua/config/dropbar.lua
+++ lua/config/dropbar.lua
@@ -21,27 +21,5 @@ require('dropbar').setup({
       'BufWinEnter',
     },
     enable = enable,
-    sources = function(buf, _)
-      local sources = require('dropbar.sources')
-      local utils = require('dropbar.utils')
-      if vim.bo[buf].ft == 'markdown' then
-        return {
-          sources.path,
-          sources.markdown,
-        }
-      end
-      if vim.bo[buf].buftype == 'terminal' then
-        return {
-          sources.terminal,
-        }
-      end
-      return {
-        sources.path,
-        utils.source.fallback({
-          sources.lsp,
-          -- sources.treesitter,
-        }),
-      }
-    end
   }
 })
